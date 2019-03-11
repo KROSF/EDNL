ca224264668301970ecb975c264009a7c58e954c
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2019-03-12 00:17:16 +0100

    change to relative path

--- tests/practica_2.cpp
+++ tests/practica_2.cpp
@@ -7,7 +7,7 @@ using namespace Vectorial;
 
 TEST(Practica_2, count_tree_nodes) {
   Abin<char> A(100);
-  std::ifstream is("/Users/krosf/Dev/uni/ednl/build/abin.txt",
+  std::ifstream is("abin.txt",
                    std::ifstream::binary);
   is >> A;
   EXPECT_EQ(12, nodos(A));
