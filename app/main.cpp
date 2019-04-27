d64c04065c3164a116a5522606b61a51e141f4ef
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2019-04-27 21:49:54 +0200

    practice 3 completed test cases

--- app/main.cpp
+++ app/main.cpp
@@ -1,18 +1,16 @@
-#include "abb.hpp"
-#include "abb_px.hpp"
-#include "ioabin.hpp"
+#include "ioagen.hpp"
+#include "agen_p1.hpp"
 #include <fstream>
 #include <iostream>
 
 using namespace Enlazada;
 
 int main() {
-  std::ifstream isa("files/abb_poda.txt", std::ifstream::binary);
-  Abin<int> A;
+  std::ifstream isa("files/agen_int.txt", std::ifstream::binary);
+  Agen<int> A;
   isa >> A;
   isa.close();
   std::cout << A << std::endl;
-  Abb<int> a{A};
-  a.podar(9);
-  std::cout << static_cast<Abin<int>>(a) << std::endl;
+  poda(A, 2);
+  std::cout << A << std::endl;
 }
--- tests/practica_3.cpp
+++ tests/practica_3.cpp
@@ -21,6 +21,25 @@ TEST_F(Practica_3, ejercicio_2_profundidad) {
   EXPECT_EQ(3, profundidad(A_, n));
 }
 
+TEST_F(Practica_3, ejercicio_2_profundidad_raiz) {
+  EXPECT_EQ(0, profundidad(A_, A_.raiz()));
+}
+
 TEST_F(Practica_3, ejercicio_3_desequilibrio) {
   EXPECT_EQ(1, desequilibrio(A_));
+}
+
+TEST_F(Practica_3, ejercicio_3_longestpath_raiz) {
+  auto n = A_.hijoIzqdo(
+      A_.hijoIzqdo(A_.hermDrcho(A_.hermDrcho(A_.hijoIzqdo(A_.raiz())))));
+  EXPECT_EQ(0, longestPath(A_, n));
+}
+
+TEST_F(Practica_3, ejercicio_4_poda) {
+  Agen<int> agen;
+  std::ifstream is("files/agen_int.txt", std::ifstream::binary);
+  is >> agen;
+  is.close();
+  poda(agen, 2);
+  EXPECT_EQ(Agen<int>::NODO_NULO, agen.hijoIzqdo(agen.hijoIzqdo(agen.raiz())));
 }
