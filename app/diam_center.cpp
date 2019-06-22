78de4fea5ee707a707e3088a05c96d052f70be60
KROSF <rodrigosanabria22@gmail.com>
2019-06-22 21:24:19 +0000

    :sparkles: Practice 8 started finished exercise 8
    started exercise 2

--- app/diam_center.cpp
+++ app/diam_center.cpp
@@ -2,19 +2,14 @@
 #include "grafos/io.hpp"
 #include "practicas/P8.hpp"
 using namespace grafos::pmc;
-using std::vector;
 using grafos::ma::Grafo;
+using std::vector;
 
 int main(void) {
   Grafo MA("files/grafos/tombuctu.txt");
   std::cout << MA << std::endl;
-  vector<CC> ciudades{{2, 4}, {-1, 5}, {2, 3}, {-8, 3}, {0, 1}, {4, -4}};
-  auto[islas, floyd] = Tombuctu(ciudades, MA);
+  vector<Ciudad> ciudades{{2, 4}, {-1, 5}, {2, 3}, {-8, 3}, {0, 1}, {4, -4}};
+  auto [islas, floyd] = Tombuctu(ciudades, MA);
   std::cout << floyd << std::endl;
-  for(auto &e : islas) {
-    for (auto& ee : e) {
-      std::cout << ee << " ";
-    }
-    std::cout << std::endl;
-  }
+  std::cout << Tombuctu2(ciudades, MA) << std::endl;
 }
--- include/practicas/P8.hpp
+++ include/practicas/P8.hpp
@@ -2,24 +2,32 @@
 #define PRACTICAS_P8_HPP
 
 #include <cmath>
+#include <numeric>
 #include "grafos/alg.hpp"
 
 using grafos::matriz;
 using grafos::Particion;
 using grafos::ma::Grafo;
-using grafos::pmc::alg::Floyd;
 using grafos::pmc::GrafoP;
+using grafos::pmc::alg::Floyd;
 using grafos::pmc::alg::vertice;
 
-struct CC {
+template <typename T>
+struct SumNotInf {
+  T operator()(T a, T b) const {
+    return ((b != GrafoP<T>::INFINITO) ? a + b : a);
+  }
+};
+
+struct Ciudad {
   double x, y;
-  double distanciaEuclidea(const CC& c) const {
-    return std::sqrt(std::pow(c.x - x, 2) - std::pow(c.y - y, 2));
+  double distancia(const Ciudad& c) const {
+    return std::sqrt(std::pow(c.x - x, 2) + std::pow(c.y - y, 2));
   }
 };
 
 std::tuple<vector<vector<size_t>>, matriz<double>> Tombuctu(
-    const vector<CC>& ciudades, const Grafo& carreteras) {
+    const vector<Ciudad>& ciudades, const Grafo& carreteras) {
   size_t n{ciudades.size()};
   Particion P{static_cast<int>(n)};
   GrafoP<double> G(n);
@@ -27,26 +35,50 @@ std::tuple<vector<vector<size_t>>, matriz<double>> Tombuctu(
   matriz<vertice<double>> FP;
   for (size_t v = 0; v < n; ++v) {
     repre_1 = P.encontrar(v);
-    for (size_t w = 0; w < n; ++w) {
+    for (size_t w = v; w < n; ++w) {
       if (carreteras[v][w]) {
         repre_2 = P.encontrar(w);
         P.unir(repre_1, repre_2);
-        G[v][w] = G[w][v] = ciudades[v].distanciaEuclidea(ciudades[w]);
+        G[v][w] = G[w][v] = ciudades[v].distancia(ciudades[w]);
       }
     }
   }
-  vector<vector<size_t>> islas;
+  repre_1 = P.encontrar(0);
+  vector<vector<size_t>> islas(1, vector<size_t>(1, 0));
   size_t n_islas{0};
-  for (size_t v = 0; v < n; ++v) {
-    repre_1 = P.encontrar(v);
-    for (size_t w = v; w < n; ++w) {
-      if (repre_1 == P.encontrar(w)) {
-        islas[n_islas].push_back(w);
-      } else {
-        ++n_islas;
-      }
+  for (size_t v = 1; v < n; ++v) {
+    if (repre_1 == P.encontrar(v)) {
+      islas[n_islas].push_back(v);
+    } else {
+      ++n_islas;
+      repre_1 = P.encontrar(v);
+      islas.push_back(vector<size_t>(1, v));
     }
   }
   return {islas, Floyd(G, FP)};
+}
+
+vector<vertice<double>> Tombuctu2(const vector<Ciudad>& ciudades,
+                                  const Grafo& carreteras) {
+  auto [islas, floyd] = Tombuctu(ciudades, carreteras);
+
+  size_t i_isla, f_isla, n_isla{islas.size()};
+  vector<vertice<double>> representante(n_isla);
+
+  double coste_min{GrafoP<double>::INFINITO}, coste_act;
+  for (size_t i = 0; i < n_isla; ++i) {
+    i_isla = islas[i][0];                    // Indice Inicio matriz isla[i]
+    f_isla = islas[i][islas[i].size() - 1];  // Indicie fin matriz isla[i]
+    for (size_t j = i_isla; j < f_isla; ++j) {
+      coste_act = std::accumulate(floyd[j].begin() + i_isla,
+                                  floyd[j].end() - (n_isla - f_isla), 0,
+                                  SumNotInf<double>());
+      if (coste_act < coste_min) {
+        coste_min = coste_act;
+        representante[i] = j;
+      }
+    }
+  }
+  return representante;
 }
 #endif
