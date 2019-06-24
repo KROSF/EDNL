601fcd765e9acd2974ca73e4c63ac381f36bbe51
KROSF <rodrigosanabria22@gmail.com>
2019-06-24 14:21:54 +0000

    :sparkles: all is good

--- app/main.cpp
+++ app/main.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
+#include "grafos/alg.hpp"
 #include "grafos/io.hpp"
-#include "practicas/P6.hpp"
 
 using namespace grafos;
 using namespace pmc;
+using alg::Dijkstra;
+using alg::DijkstraInv;
+using alg::vertice;
 using std::cout;
 using std::endl;
 using std::vector;
 
+template <typename C>
+C UnSoloTransbordo(const GrafoP<C>& tren, const GrafoP<C>& bus,
+                   vertice<C> origen, vertice<C> destino) {
+  vector<vertice<C>> P;
+  vector<C> DTren{Dijkstra(tren, origen, P)};
+  vector<C> DInvTren{DijkstraInv(tren, destino, P)};
+  vector<C> DBus{Dijkstra(bus, origen, P)};
+  vector<C> DInvBus{DijkstraInv(bus, destino, P)};
+  const size_t n = tren.numVert();
+  vector<C> min_elem(2 * n);
+  for (size_t i = 0; i < n; ++i) {
+    min_elem[i] = DTren[i] + DInvBus[i];
+    min_elem[n + i] = DBus[i] + DInvTren[i];
+  }
+  cout << DTren << endl
+       << DInvTren << endl
+       << DBus << endl
+       << DInvBus << endl
+       << min_elem << endl;
+  return *std::min_element(min_elem.begin(), min_elem.end());
+}
+
 int main(void) {
-  GrafoP<int> G("files/grafos/dirigido3.txt");
-  cout << std::boolalpha << esAciclico(G) << endl;
-  matriz<alg::vertice<int>> P;
-  matriz<int> F{alg::Floyd(G, P)};
-  cout << G << endl;
-  cout << P << endl;
-  cout << F << endl;
+  GrafoP<int> B("files/grafos/bus.txt");
+  GrafoP<int> T("files/grafos/tren.txt");
+  cout << UnSoloTransbordo(T, B, 0, 7);
 }
--- include/practicas/P8.hpp
+++ include/practicas/P8.hpp
@@ -2,6 +2,7 @@
 #define PRACTICAS_P8_HPP
 
 #include <cmath>
+#include <map>
 #include <numeric>
 #include "grafos/alg.hpp"
 
@@ -11,6 +12,7 @@ using grafos::ma::Grafo;
 using grafos::pmc::GrafoP;
 using grafos::pmc::alg::arista;
 using grafos::pmc::alg::Floyd;
+using grafos::pmc::alg::KruskallMax;
 using grafos::pmc::alg::vertice;
 
 template <typename T>
@@ -67,41 +69,34 @@ vector<arista<double>> Tombuctu2(const vector<Ciudad>& ciudades,
   int repre_1, repre_2;
   for (size_t v = 0; v < n; ++v) {
     repre_1 = P.encontrar(v);
-    for (size_t w = 0; w < n; ++w) {
+    for (size_t w = v; w < n; ++w) {
       if (carreteras[v][w]) {
         repre_2 = P.encontrar(w);
         P.unir(repre_1, repre_2);
       }
-      G[v][w] = ciudades[v].distancia(ciudades[w]);
+      G[v][w] = G[w][v] = ciudades[v].distancia(ciudades[w]);
     }
   }
-  repre_1 = P.encontrar(0);
-  vector<vector<vertice<size_t>>> islas(1, vector<size_t>(1, 0));
-  size_t n_islas{0};
-  for (size_t i = 1; i < n; ++i) {
-    if (repre_1 == P.encontrar(i))
-      islas[n_islas].push_back(i);
-    else {
-      ++n_islas;
-      repre_1 = P.encontrar(i);
-      islas.push_back(vector<size_t>(1, i));
-    }
+  std::map<int, vector<size_t>> islas;
+  for (size_t i = 0; i < n; ++i) {
+    islas[P.encontrar(i)].push_back(i);
   }
   matriz<vertice<double>> mP;
   matriz<double> floyd{Floyd(G, mP)};
   double coste_act, costes_min{GrafoP<double>::INFINITO};
-  n_islas = islas.size();
+  size_t n_islas = islas.size();
   vector<vertice<double>> repre(n_islas);
-  for (size_t i = 0; i < n_islas; ++i) {
-    for (size_t j = islas[i][0]; j <= islas[i][islas[i].size() - 1];
-         ++j, costes_min = GrafoP<double>::INFINITO) {
-      coste_act = std::accumulate(floyd[j].begin(), floyd[j].end(), 0,
-                                  SumNotInf<double>());
+  int repre_i = 0;
+  for (auto& isla : islas) {
+    for (auto& ciudad : isla.second) {
+      coste_act =
+          std::accumulate(floyd[ciudad].begin(), floyd[ciudad].end(), 0);
       if (coste_act < costes_min) {
         costes_min = coste_act;
-        repre[i] = j;
+        repre[repre_i] = ciudad;
       }
     }
+    ++repre_i;
   }
   vector<arista<double>> aerolinea;
   for (size_t i = 0; i < n_islas; ++i) {
@@ -112,5 +107,34 @@ vector<arista<double>> Tombuctu2(const vector<Ciudad>& ciudades,
     }
   }
   return aerolinea;
+}
+
+template <typename T>
+vector<arista<T>> Emasajer(const GrafoP<T>& G) {
+  GrafoP<T> K{KruskallMax(G)};
+  vector<arista<T>> canales;
+  size_t n = K.numVert();
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = i + 1; j < n; ++j) {
+      if (K[i][j] != GrafoP<T>::INFINITO) {
+        canales.push_back(arista<T>{i, j, K[i][j]});
+      }
+    }
+  }
+  return canales;
+}
+
+template <typename T>
+T Reteuni3(const GrafoP<T>& pais) {
+  matriz<vertice<T>> mP;
+  matriz<T> floyd{Floyd(pais, mP)};
+  T coste_min{GrafoP<T>::INFINITO}, coste_act;
+  for (size_t i = 0; i < floyd.dimension(); ++i) {
+    coste_act = accumulate(floyd[i].begin(), floyd[i].end(), 0, SumNotInf<T>());
+    if (coste_act < coste_min) {
+      coste_min = coste_act;
+    }
+  }
+  return coste_min;
 }
 #endif
